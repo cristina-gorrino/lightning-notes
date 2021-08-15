a40f06233966e74391909b360ed56b0c0d9e9421
cgorrino <cgorrino03@gmail.com>
2021-08-15 13:37:36 -0700

    Adding logic to add user and login user in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,32 @@
-const { Tech, Matchup } = require('../models');
+const { User } = require('../models');
 
 const resolvers = {
   Query: {
     
   },
   Mutation: {
-    
+    addUser: async (parent, {email, password }) => {
+      const user = await User.create({ email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    }
   },
 };
 
