a508f822932e15cd32205b2f5979a54b5cabfc62
cgorrino <cgorrino03@gmail.com>
2021-08-22 02:04:46 -0700

    Redirect to dashboard after you add a category

--- client/src/components/Notes/CategoryForm.js
+++ client/src/components/Notes/CategoryForm.js
@@ -1,8 +1,10 @@
 import { useMutation } from "@apollo/client";
 import React, { useState } from "react";
 import { ADD_CATEGORY } from "../../utils/mutations";
+import { useHistory } from "react-router-dom";
 
 const CategoryForm = () => {
+  const history = useHistory();
   const [addCategory, { error }] = useMutation(ADD_CATEGORY);
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -12,6 +14,8 @@ const CategoryForm = () => {
         variables: { name: newCategory },
       });
 
+      
+      history.push("/");
       window.location.reload();
     } catch (err) {
       console.error(err);
