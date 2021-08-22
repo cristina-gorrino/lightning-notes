fa3c59ed636f8d0ae7fadcf6bbb11dadfbd4af6d
cgorrino <cgorrino03@gmail.com>
2021-08-21 23:51:02 -0700

    Querying real category data on dashboard page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 import Button from '@material-ui/core/Button';
 
 import Category from "../components/Notes/Category";
+import { useQuery } from '@apollo/client';
+import { QUERY_CATEGORIES } from '../utils/queries';
 
 
 
@@ -54,14 +56,49 @@ const Home = () => {
   ]);
   //const classes = useStyles();
 
+  const { loading, data } = useQuery( QUERY_CATEGORIES);
+  const categories = data?.categories || [];
+  console.log(data);
+  if (!categories.length) {
+    return <h3>No categories yet ...</h3>
+  }
+
   return (
     <main>
       
         <div>
-        {useCategory.map((category) => (
+        {loading ? (
+            <div>Loading...</div>
+          ) : (
+            
+              categories.map((category) => {
+                if (category.name === "Home Task") {
+                  category.bgColor = {backgroundColor: '#DAF7A6'}
+                } else if (category.name === "Work Task") {
+                  category.bgColor = {backgroundColor: '#FFC300'}
+                } else if (category.name === "School Task") {
+                  category.bgColor = {backgroundColor: '#33E0FF'}
+                } else if (category.name === "Shopping Task") {
+                  category.bgColor = {backgroundColor: '#D8A6DF'}
+                } else if (category.name === "Other Task") {
+                  category.bgColor = {backgroundColor: '#F5CBA7'}
+                }
+
+
+              <Category
+              key={category._id}
+              id={category._id}
+              name={category.name}
+              bgColor={category.bgColor}
+              />
+              }) 
+              
+            
+          )}
+        {/* {useCategory.map((category) => (
 
           <Category name = { category.categoryName } key={category.id} bgColor={category.bgColor}/>
-        ))}
+        ))} */}
         </div>
         <div style={buttonStyle}>
           <Button variant="contained">Select a new category</Button>
